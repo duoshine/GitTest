213484e2cd8e092843651c5ad12917385aa2578a
duoshine.chen <duoshine.chen@cenhotec.com>
2022-07-20 15:48:29 +0800

    master修改

--- app/src/main/java/com/duoshine/gittest/MainActivity.java
+++ app/src/main/java/com/duoshine/gittest/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * 想倒回某个提交编版本使用 Keep
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
